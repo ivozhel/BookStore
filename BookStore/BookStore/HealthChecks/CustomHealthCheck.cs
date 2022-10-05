91aae4e0bfd0418bcb60329018adbc57a490246e
i.zhelyazkov <ivozhel@gmail.com>
2022-10-05 11:59:57 +0300

    Health Checks

---
+++ BookStore/BookStore/HealthChecks/CustomHealthCheck.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.SqlClient;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace BookStore.HealthChecks
+{
+    public class CustomHealthCheck : IHealthCheck
+    {
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                //throw new NotImplementedException();
+            }
+            catch (SqlException e)
+            {
+                return HealthCheckResult.Unhealthy(e.Message);
+            }
+            return HealthCheckResult.Healthy();
+        }
+    }
+}
